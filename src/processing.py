7bd6e4a0c5f23b2e09b16966de5455344af2b61e
Alexander <Alex_katunin@vk.com>
2025-06-13 16:46:54 +0300

    update_project_lint

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,18 @@
-def filter_by_state(operations, state='EXECUTED'):
+from typing import Any
+
+
+def filter_by_state(operations: list[dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
     """
     Фильтрует список операций по значению ключа 'state'.
 
     """
     filtered_operations = []
     for operation in operations:
-        if 'state' in operation and operation['state'] == state:
+        if "state" in operation and operation["state"] == state:
             filtered_operations.append(operation)
     return filtered_operations
 
 
-def sort_by_date(operations, reverse=True):
-    '''Функция сортирует список по дате'''
-    return sorted(operations, key=lambda x: x.get('date', ''), reverse=reverse)
+def sort_by_date(operations: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
+    """Функция сортирует список по дате"""
+    return sorted(operations, key=lambda x: x.get("date", ""), reverse=reverse)
