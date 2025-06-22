3c65c86ef6cd15160a43ca4003a0bcd1f677fec7
Alexander <Alex_katunin@vk.com>
2025-06-22 23:14:47 +0300

    update test_widget.py

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,23 +1,18 @@
 import pytest
 from src.widget import mask_account_card, get_date
 
+
 @pytest.mark.parametrize("info, expected", [
     ("Счет 12345678901234561234", "Счет **1234"),
     ("Счет 12345678901234567890", "Счет **7890"),
-
+    ('Visa Platinum 7000792289606361', 'Visa Platinum 7000 79** **** 6361'),
+    ('Maestro 7000792289606361', 'Maestro 7000 79** **** 6361')
 ])
-def test_mask_account_card(info, expected):
+def test_mask_account_card(info: str, expected: str) -> None:
+    """Тест маскировки счета или номера карты"""
     assert mask_account_card(info) == expected
 
-def test_get_date():
+
+def test_get_date() -> None:
     """Тест преобразования строки даты"""
     assert get_date("2019-07-03T18:35:29.512364") == "03.07.2019"
-
-
-
-def test_get_date_invalid():
-    with pytest.raises(ValueError):
-        get_date("invalid-date")
-
-    with pytest.raises(ValueError):
-        get_date("")
